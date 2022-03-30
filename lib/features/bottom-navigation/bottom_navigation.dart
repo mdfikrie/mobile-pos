7fea5828d3b5e7872f7a7e5cff8e5994389a95e9
mdfikrie <mdzulfikrikh@gmail.com>
2022-03-31 00:27:13 +0700

    add search

--- lib/features/bottom-navigation/bottom_navigation.dart
+++ lib/features/bottom-navigation/bottom_navigation.dart
@@ -8,44 +8,46 @@ class BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final pageController = PageController();
-    return Scaffold(
-      body: PageView(
-        physics: NeverScrollableScrollPhysics(),
-        controller: pageController,
-        children: [
-          HomeScreen(),
-          HomeScreen(),
-          HomeScreen(),
-          HomeScreen(),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: primaryGreen,
-        selectedLabelStyle: TextStyle(color: Colors.blue, fontSize: 12),
-        unselectedLabelStyle: TextStyle(color: Colors.grey, fontSize: 12),
-        onTap: (value) {
-          pageController.jumpToPage(value);
-        },
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorite',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          physics: NeverScrollableScrollPhysics(),
+          controller: pageController,
+          children: [
+            HomeScreen(),
+            HomeScreen(),
+            HomeScreen(),
+            HomeScreen(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: primaryGreen,
+          selectedLabelStyle: TextStyle(color: Colors.blue, fontSize: 12),
+          unselectedLabelStyle: TextStyle(color: Colors.grey, fontSize: 12),
+          onTap: (value) {
+            pageController.jumpToPage(value);
+          },
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_pos/const/color.dart';
+import 'package:mobile_pos/features/home/widgets/search.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,8 +8,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: Column(
-        children: [],
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'My Cafe',
+                    style: TextStyle(
+                      color: primaryGreen,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Search(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
